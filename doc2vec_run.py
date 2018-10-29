251fc49c1f535cade2a52e51df633fc67c9ceb22
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2018-10-29 11:51:29 -0400

    Changes doc2vec

--- doc2vec_run.py
+++ doc2vec_run.py
@@ -6,7 +6,7 @@ from gensim.test.test_doc2vec import ConcatenatedDoc2Vec
 from random import shuffle
 from collections import OrderedDict
 import multiprocessing
-import os
+import os, sys
 import pdb
 import pandas as pd
 
@@ -21,14 +21,31 @@ class DataLoader():
         self.tagged_doc = None
         self.data = None
 
-    def load_data(self, data_fpath):
-        self.data_fpath = os.path.join(self.base_dirpath, data_fpath)
-        self.data = pd.read_pickle(self.data_fpath)
+    def load_data(self, data_fpath, data_name, multi=False):
+        self.data_name = data_name
+        if multi:
+            self.data_fpath = [os.path.join(self.base_dirpath, data_fpath.format(name)) for name in data_name]
+        else:
+            self.data_fpath = os.path.join(self.base_dirpath, data_fpath.format(data_name))
+        if multi:
+            self.data = pd.concat([pd.read_pickle(f) for f in self.data_fpath])
+        else:
+            self.data = pd.read_pickle(self.data_fpath)
 
     def build_tagged_docs(self, text_colname, labels_colname):
-        alldocs = []
+        #self.data.fillna(value={text_colname: '', labels_colname: [None]}, inplace=True)
+        #self.data.fillna(value={text_colname: ''}, inplace=True)
+
+        # Get rid of NAN rows
+        self.data.dropna(subset=[text_colname, labels_colname], how='all', inplace=True)
+        print(f"{len(self.data)} rows of data considered.")
+
+        alldocs = []    
+
         for line, tags in zip(self.data[text_colname], self.data[labels_colname]):
-            tokens = gensim.utils.to_unicode(line).split()
+            if isinstance(tags, float):
+                pdb.set_trace()
+            tokens = gensim.utils.to_unicode(str(line)).split()
             alldocs.append(TaggedDocument(tokens, tags))
 
         doc_list = alldocs[:]  
@@ -43,6 +60,7 @@ class ModelTrainer():
         self.cores = cores
         self.models = []
         self.data_loader = data_loader
+        self.data_name = self.data_loader.base_dirpath
         self.model_dirpath = os.path.join(data_loader.base_dirpath, 'models')
         pass
 
@@ -65,24 +83,38 @@ class ModelTrainer():
             model.build_vocab(self.data_loader.tagged_doc)
             print("%s vocabulary scanned & state initialized" % model)
 
-    def train_models(self):
+    def train_models(self, model_name):
         for model in self.models: 
             print("Training %s" % model)
             model.train(self.data_loader.tagged_doc, total_examples=len(self.data_loader.tagged_doc), epochs=model.epochs) # Adjust epochs in model spec
 
             print("Saving %s" % model)
-            model.save(os.path.join(self.model_dirpath, f"{model.comment}.model"))
+            model_outdir = os.path.join(self.model_dirpath, model_name)
+            if not os.path.exists(model_outdir):
+                os.makedirs(model_outdir)
+            model_outpath = os.path.join(model_outdir, f"{model.comment}.model")
+            model.save(model_outpath)
+            print("Saved to %s" % model_outpath)
 
 def main():
     base_dirpath = '/usr2/mamille2/fanfiction-project'
-    data_fpath = 'data/ao3/friends/friends_discoursedb_data.pkl'
+    #data_name = 'detroit'
+    data_names = ['academia', 'detroit', 'friends']
+    data_format_fpath = 'data/ao3/{0}/{0}_discoursedb_data.pkl'
+    multi = True
+
+    print("Loading data...", end='')
+    sys.stdout.flush()
     data_loader = DataLoader(base_dirpath)
-    data_loader.load_data(data_fpath)
-    data_loader.build_tagged_docs('preprocessed_content', 'relationship_type')
+    data_loader.load_data(data_format_fpath, data_names, multi=multi)
+    #data_loader.build_tagged_docs('preprocessed_content', 'relationship_type')
+    data_loader.build_tagged_docs('preprocessed_text', 'category')
 
     model_trainer = ModelTrainer(data_loader)
     model_trainer.build_models()
-    model_trainer.train_models()
+    model_name = 'academia-detroit-friends'
+    #model_name = data_loader.data_name
+    model_trainer.train_models(model_name)
 
     # models_by_name = OrderedDict((str(model), model) for model in simple_models)
     # models_by_name['dbow+dmm'] = ConcatenatedDoc2Vec([simple_models[0], simple_models[1]])
