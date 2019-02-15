643fe219946ae04156fecd708d94eac7a772bc92
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2019-02-15 16:57:48 -0500

    ACL experiments

--- preprocessing_for_embedding_training.py
+++ preprocessing_for_embedding_training.py
@@ -20,12 +20,13 @@ data_dirpath_template = '/usr2/scratch/fanfic/ao3_{}_text/stories'
 fic_metadata = '/usr0/home/prashang/DirectedStudy/ACL_preprocessing/stories_kudos50.csv'
 output_dirpath = '/usr2/mamille2/fanfiction-project/data/ao3/{0}'
 
-nlp = spacy.load('en', disable=['ner'])
+#nlp = spacy.load('en', disable=['ner'])
+nlp = spacy.load('en', disable=['parser', 'ner']) # 1 para/line
 
 for fandom in fandoms:
     print(fandom)
     fandom_dirpath = data_dirpath_template.format(fandom)
-    output_fpath = os.path.join(output_dirpath.format(fandom), '{0}_sentences.txt')
+    output_fpath = os.path.join(output_dirpath.format(fandom), '{0}_tokenized_paras.txt')
     
     if not os.path.exists(output_dirpath.format(fandom)):
         os.makedirs(output_dirpath.format(fandom))
@@ -39,23 +40,24 @@ for fandom in fandoms:
 
     #for fname in tqdm(os.listdir(fandom_dirpath)):
     for fname in tqdm(fic_fnames):
-        #paras = pd.read_csv(os.path.join(fandom_dirpath, fname))['text'].tolist()
-        paras = ' '.join([p for p in pd.read_csv(os.path.join(fandom_dirpath, fname))['text'].tolist() if isinstance(p, str)])
 
-        # spaCy sentence tokenizer
-        doc = nlp(paras)
+        # 1 paragraph/line
+        paras = [p for p in pd.read_csv(os.path.join(fandom_dirpath, fname))['text'].tolist() if isinstance(p, str)]
+
+        # 1 sentence/line
+        #paras = ' '.join([p for p in pd.read_csv(os.path.join(fandom_dirpath, fname))['text'].tolist() if isinstance(p, str)])
+        #doc = nlp(paras)
 
         with open(output_fpath.format(fname[:-4]), 'w') as fil:
-            for sent in doc.sents:
-                toks_str = ' '.join([tok.text for tok in sent])
-                fil.write(f"{toks_str}\n")
-          
-            #for para in paras: # might be faster to join
-            #    if not isinstance(para, str):
-            #        continue
-            #    doc = nlp(para)
 
-            #    for sent in doc.sents:
-            #        toks_str = ' '.join([tok.text for tok in sent])
-            #        pdb.set_trace()
-            #        fil.write(f"{toks_str}\n")
+            # 1 para/line
+            for para in paras:
+                doc = nlp(para)
+                toks_str = ' '.join([tok.text for tok in doc])
+                fil.write(f"{toks_str}\n")
+
+            # 1 sentence/line
+#            for sent in doc.sents:
+#                toks_str = ' '.join([tok.text for tok in sent])
+#                fil.write(f"{toks_str}\n")
+          
