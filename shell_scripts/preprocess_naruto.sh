d6b9bfaa450e2369951b7ab4fe53fd2408a53347
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2021-01-08 17:18:08 -0500

    more processing of fandoms

--- shell_scripts/preprocess_naruto.sh
+++ shell_scripts/preprocess_naruto.sh
@@ -1,2 +1,3 @@
-fandom=startrek
+fandom=naruto
+python /projects/fanfiction_lgbtq/scripts/filter_fics.py $fandom complete_en_1k-50k
 python /projects/fanfiction_lgbtq/scripts/preprocess_fics.py /projects/AO3Scraper/output/ao3_${fandom}_text /data/fanfiction_ao3/${fandom}/complete_en_1k-50k/ --update-existing --num-copy-workers 1 --num-tok-workers 30
--- shell_scripts/preprocess_once.sh
+++ shell_scripts/preprocess_once.sh
@@ -1,2 +1,2 @@
-fandom=startrek
+fandom=once
 python /projects/fanfiction_lgbtq/scripts/preprocess_fics.py /projects/AO3Scraper/output/ao3_${fandom}_text /data/fanfiction_ao3/${fandom}/complete_en_1k-50k/ --update-existing --num-copy-workers 1 --num-tok-workers 30
