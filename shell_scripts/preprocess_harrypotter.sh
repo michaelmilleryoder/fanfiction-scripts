51ce67b7f428f765e86ead4855a8e94da6eb350c
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2021-01-21 08:59:53 -0500

    added regression lines to plots

---
+++ shell_scripts/preprocess_harrypotter.sh
@@ -0,0 +1,2 @@
+fandom=harrypotter
+python /projects/fanfiction_lgbtq/scripts/preprocess_fics.py /projects/AO3Scraper/output/ao3_${fandom}_text /data/fanfiction_ao3/${fandom}/complete_en_1k-50k/ --update-existing --num-copy-workers 1 --num-tok-workers 30
